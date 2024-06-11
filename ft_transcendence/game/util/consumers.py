612f019b9bea0f729be9a2ee375819ad9049e9da
chanson <shhmun02@naver.com>
2024-06-11 13:48:50 +0900

    2vs2

--- ft_transcendence/game/util/consumers.py
+++ ft_transcendence/game/util/consumers.py
@@ -10,9 +10,23 @@ s2 = 0
 dt = 0
 lastTime = 0
 players = 0
-b = ball.Ball()
-p1 = ball.Stick([-15,0,0])
-p2 = ball.Stick([15,0,0])
+b = ball.Ball(0.5)
+p1 = ball.Stick([-15,1.5,0], 0.5, 3)
+p2 = ball.Stick([-15,-1.5,0], 0.5, 3)
+p3 = ball.Stick([15, 1.5,0], 0.5, 3)
+p4 = ball.Stick([15, -1.5, 0], 0.5, 3)
+paddles = []
+paddles.append(p1)
+paddles.append(p2)
+paddles.append(p3)
+paddles.append(p4)
+obtacles = []
+obtacles.append(ball.Box(30, 0.5))
+obtacles[0].movePos([0, 8, 0])
+obtacles.append(ball.Box(30, 0.5))
+obtacles[1].movePos([0, -8, 0])
+obtacles[1].rotBox(0)
+
 class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         await self.accept()
@@ -22,10 +36,18 @@ class GameConsumer(AsyncWebsocketConsumer):
         global b
         global p1
         global p2
+        global p3
+        global p4
+        global paddles
+        global obtacles
+        global s1
+        global s2
         
         ball_pos = b.pos
         paddle1_pos = p1.pos
         paddle2_pos = p2.pos
+        paddle3_pos = p3.pos
+        paddle4_pos = p4.pos
         players += 1
         players %= 3
         if players == 0:
@@ -35,6 +57,10 @@ class GameConsumer(AsyncWebsocketConsumer):
             'ball_pos': ball_pos,
             'paddle1_pos': paddle1_pos,
             'paddle2_pos': paddle2_pos,
+            'paddle3_pos': paddle3_pos,
+            'paddle4_pos': paddle4_pos,
+            'score1': s1,
+            'score2': s2
             }))
 
     async def disconnect(self, close_code):
@@ -48,17 +74,29 @@ class GameConsumer(AsyncWebsocketConsumer):
         global b
         global p1
         global p2
+        global p3
+        global p4
+        global paddles
+        global obtacles
+        global players
         while True:
             dt = (time.perf_counter() - lastTime)
             lastTime = time.perf_counter()
 
-            p1.update(p1.dir[1] * 10 * dt)
-            p2.update(p2.dir[1] * 10 * dt)
-            b.update(p1, p2, dt * 20)
+            map_length = obtacles[0].bot1[1] - obtacles[1].top1[1]
+            p1.update(p1.dir[1] * 10 * dt, obtacles[0].bot1[1], obtacles[1].top1[1] + map_length / 2)
+            p2.update(p2.dir[1] * 10 * dt, obtacles[0].bot1[1], obtacles[1].top1[1] + map_length / 2)
+            p3.update(p3.dir[1] * 10 * dt, obtacles[0].bot1[1] - map_length / 2, obtacles[1].top1[1])
+            p4.update(p4.dir[1] * 10 * dt, obtacles[0].bot1[1] - map_length / 2, obtacles[1].top1[1])
+            b.pos[2] = 1
+            flag = b.update(paddles, obtacles, dt * 20)
+            b.pos[2] = 0
             
             ball_pos = b.pos
             paddle1_pos = p1.pos
             paddle2_pos = p2.pos
+            paddle3_pos = p3.pos
+            paddle4_pos = p4.pos
             score1 = b.point1
             score2 = b.point2
             # 클라이언트로 메시지 보내기
@@ -66,8 +104,11 @@ class GameConsumer(AsyncWebsocketConsumer):
             'ball_pos': ball_pos,
             'paddle1_pos': paddle1_pos,
             'paddle2_pos': paddle2_pos,
+            'paddle3_pos': paddle3_pos,
+            'paddle4_pos': paddle4_pos,
             'score1': score1,
             'score2': score2,
+            'players': players
             }))
             # 초 대기
             await asyncio.sleep(0.001)
@@ -79,19 +120,19 @@ class GameConsumer(AsyncWebsocketConsumer):
 
         if player is not None:
             player = int(player)
-        if (message == '1pup' and player % 2 == 1):
+        if (message == 'up' and player % 2 == 1):
             p1.dir[1] = 1
-        if (message == '1pdown' and player % 2 == 1):
+        if (message == 'down' and player % 2 == 1):
             p1.dir[1] = -1
-        if (message == '2pup' and player % 2 == 0):
+        if (message == 'up' and player % 2 == 0):
             p2.dir[1] = 1
-        if (message == '2pdown' and player % 2 == 0):
+        if (message == 'down' and player % 2 == 0):
             p2.dir[1] = -1
-        if (message == '1pupstop' and p1.dir[1] == 1 and player % 2 == 1):
+        if (message == 'upstop' and p1.dir[1] == 1 and player % 2 == 1):
             p1.dir[1] = 0
-        if (message == '1pdownstop' and p1.dir[1] == -1 and player % 2 == 1):
+        if (message == 'downstop' and p1.dir[1] == -1 and player % 2 == 1):
             p1.dir[1] = 0
-        if (message == '2pupstop' and p2.dir[1] == 1 and player % 2 == 0):
+        if (message == 'upstop' and p2.dir[1] == 1 and player % 2 == 0):
             p2.dir[1] = 0
-        if (message == '2pdownstop' and p2.dir[1] == -1 and player % 2 == 0):
+        if (message == 'downstop' and p2.dir[1] == -1 and player % 2 == 0):
             p2.dir[1] = 0
